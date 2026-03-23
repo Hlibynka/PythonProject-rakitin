169590fa4b00e6291fbce97d69ce98785c63c9f1
Your Name <stas101203rakitin@gmail.com>
2026-03-23 11:57:00 +0200

    refactor core logic

--- math_logic.py
+++ math_logic.py
@@ -1,7 +1,7 @@
-def add(a, b):
+def add(a: float, b: float) -> float:
     return a + b
 
-def divide(a, b):
+def divide(a: float, b: float) -> float:
     if b == 0:
-        return 0
+        raise ValueError("Ділення на нуль неможливе")
     return a / b

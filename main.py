d45947e3845c247c7f6686d50566301bb19dc0bd
Your Name <stas101203rakitin@gmail.com>
2026-03-23 11:57:24 +0200

    fix bug

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math_logic
+from validation import is_valid_number
 
 a = input("Введіть перше число: ")
 b = input("Введіть друге число: ")
 
-print("Результат додавання:", math_logic.add(int(a), int(b)))
+if is_valid_number(a) and is_valid_number(b):
+    print("Результат додавання:", math_logic.add(float(a), float(b)))
+else:
+    print("Помилка: введено некоректні дані! Будь ласка, вводьте лише числа.")
